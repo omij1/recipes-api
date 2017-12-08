6c8ad3d43e5b35defe9e218b7d2c137be625c295
Rafael <rafahidalgo.83@gmail.com>
2017-12-08 22:14:29 +0100

    Implementación básica de los action methods retrieveUser, updateUser, deleteUser, retrieveUserCollection

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,13 +1,17 @@
 package controllers;
 
+
+import io.ebean.PagedList;
 import models.User;
 import play.data.Form;
 import play.data.FormFactory;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class UserController extends Controller {
 
@@ -59,47 +63,143 @@ public class UserController extends Controller {
     }
 
     /**
+     * Método para obtener los datos de un usuario
+     *
      * @param nick Nombre del usuario del que se quiere obtener la información
      * @return Indica si se ha realizado correctamente o no la operación
      */
     public Result retrieveUser(String nick) {
 
-        //TODO Comprobar que el usuario existe, el formato y el idioma de la respuesta
-        return Results.ok();
+        User user = User.findByNick(nick);
+
+        /**
+         * Si el nick no existe, se devuelve un error
+         */
+        if (user == null) {
+            return Results.notFound(); //TODO cambiar cuando se implemente el objeto error
+        }
+
+        /**
+         * Formato de respuesta dependiendo de que lo que acepte la petición
+         */
+        if (request().accepts("application/xml")) {
+            return ok(views.xml.user.render(user));
+        } else if (request().accepts("application/json")) {
+            return ok(Json.toJson(user));
+        } else {
+            //TODO cambiar cuando se implemente el objeto Error
+            return status(415); //Unsupported media type
+        }
+
+
+        //TODO idioma de la respuesta
     }
 
     /**
+     * Método para actualizar los datos de un usuario
+     *
      * @param nick Nombre del usuario del que se quiere realizar una modificación de los datos
      * @return Indica si se ha realizado correctamente o no la operación
      */
     public Result updateUser(String nick) {
 
-        //TODO Comprobar que el usuario existe. Sólo pueden modificar los datos de un usuario el propio usuario o el administrador
-        return Results.ok();
+        /**
+         * Creación de objeto Form para obtener los datos de la petición
+         */
+        Form<User> f = formFactory.form(User.class).bindFromRequest();
+
+        //Comprobar si hay errores
+        if (f.hasErrors()) {
+            //TODO Crear objeto Error
+            return ok(f.errorsAsJson());
+        }
+        /**
+         * Objeto User donde se guarda la información de la petición
+         */
+        User updateUser = f.get();
+        User user = User.findByNick(nick);
+
+        /**
+         * Comprobar si existe el usuario con el nick indicado
+         */
+        if (user == null) {
+            return Results.notFound();
+        }
+
+        /**
+         * Si existe, asignamos el id del usuario a los nuevos datos y actualizamos
+         */
+        updateUser.setId_user(user.getId_user());
+        updateUser.update();
+        return ok();
+
+        //TODO Sólo pueden modificar los datos de un usuario el propio usuario o el administrador
     }
 
     /**
+     * Método para borrar un usuario
+     *
      * @param nick Nombre del usuario que se quiere borrar
      * @return Indica si se ha realizado correctamente o no la operación
      */
     public Result deleteUser(String nick) {
 
-        //TODO Comprobar que el usuario existe y borrar. Aunque ya no exista se debe dar una respuesta correcta (idempotencia)
+        User user = User.findByNick(nick);
+        /**
+         * Si el usuario existe, borrar
+         */
+        if (user != null) {
+            if (user.delete()) {
+                return ok();
+            } else {
+                return Results.internalServerError("Error al eliminar usuario"); //TODO cambiar cuando se implemente el objeto Error
+            }
+        }
+
+        /**
+         * Por idempotencia, aunque no exista el usuario, la respuesta debe ser correcta.
+         */
+        return ok();
+
+
         //TODO Sólo pueden borrar un usuario el propio usuario y el administrador
-        return Results.ok();
     }
 
 
-    //TODO PENDIENTE DE APROBACIÓN
-
     /**
+     * Método para obtener un listado de los usuarios
+     *
      * @param page Página que se quiere obtener
      * @return Indica si se ha realizado correctamente o no la operación
      */
     public Result retrieveUserCollection(Integer page) {
 
-        //TODO Comprobar el formato y el idioma de la respuesta
-        return Results.ok();
+        /**
+         * Creamos un objeto de la clase PagedList para obtener la lista
+         */
+        PagedList<User> list = User.findAll(page);
+        List<User> usersList = list.getList();
+
+        if (usersList.isEmpty()) {
+            if (request().accepts("application/xml")) {
+                return ok(views.xml.users.render(usersList));
+            } else if (request().accepts("application/json")) {
+                return ok(Json.toJson(usersList));
+            } else {
+                return status(415); //Unsupported media type
+            }
+        }
+
+        if (request().accepts("application/xml")) {
+            return ok(views.xml.users.render(usersList));
+        } else if (request().accepts("application/json")) {
+            return ok(Json.toJson(usersList));
+        }
+
+        return status(415); //Unsupported media type
+
+
+        //TODO Comprobar el idioma de la respuesta
     }
 
 
